c80704307e4a0bb44c4a40026c3dce6ff7c241f5
Audra Tallman <audra.tallman@hotmail.com>
2019-03-13 22:17:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,11 +19,8 @@ def take_a_number (katz_deli, name)
 end  
 
 def now_serving(katz_deli)
-  if katz_deli.empty?
- puts "There is nobody waiting to be served!"
- else
-  puts "Currently serving #{katz_deli.first}."
-  katz_deli.shift
+  if katz_deli.empty? "There is nobody waiting to be served!"
+ else puts "Currently serving #{katz_deli.first}."
  end
 end
 
